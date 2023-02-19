c926d1841fdb850bc6df28e1b6d9bbd278a8d8b9
Vishesh Singh <VisheshSingh16@gmail.com>
2023-02-18 23:32:09 -0500

    create jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,14 +1,21 @@
 const User = require('../models/userModel');
+const jwt = require('jsonwebtoken');
+
+const createToken = (_id) => {
+  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '1d' });
+};
 
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
   try {
     const user = await User.login(email, password);
+
     res.status(200).json(user);
   } catch (error) {
     res.status(400).json({ error: error.message });
   }
 };
+
 const signupUser = async (req, res) => {
   const { name, email, password } = req.body;
   try {
