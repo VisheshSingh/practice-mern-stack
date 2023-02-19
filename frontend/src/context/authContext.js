46baa6228b9daf17654ebb746453940967f2b9b6
Vishesh Singh <VisheshSingh16@gmail.com>
2023-02-19 08:37:07 -0500

    auth context

---
+++ frontend/src/context/authContext.js
@@ -0,0 +1,28 @@
+import { createContext, useReducer } from 'react';
+
+const authReducer = (state, action) => {
+  switch (action.type) {
+    case 'LOGIN':
+      return { user: action.payload };
+    case 'LOGOUT':
+      return { user: null };
+    default:
+      return state;
+  }
+};
+
+const AuthContext = createContext();
+
+export const AuthContextProvider = (children) => {
+  const [state, dispatch] = useReducer(authReducer, {
+    user: null,
+  });
+
+  return (
+    <AuthContext.Provider value={{ ...state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export default AuthContext;
